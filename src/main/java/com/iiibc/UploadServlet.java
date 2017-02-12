02d05f477a568dd6c2bb0432c413bbe4e0d891ed
ben gao <baiyun.gao@exostar.com>
2017-02-12 14:23:02 -0500

    init checkin

---
+++ src/main/java/com/iiibc/UploadServlet.java
@@ -0,0 +1,165 @@
+package com.iiibc;
+
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.*;
+
+import javax.servlet.ServletException;
+        import javax.servlet.annotation.MultipartConfig;
+        import javax.servlet.annotation.WebServlet;
+        import javax.servlet.http.HttpServlet;
+        import javax.servlet.http.HttpServletRequest;
+        import javax.servlet.http.HttpServletResponse;
+        import javax.servlet.http.Part;
+
+
+
+@WebServlet("/UploadServlet")
+@MultipartConfig(fileSizeThreshold=1024*1024*2, // 2MB
+        maxFileSize=1024*1024*10,      // 10MB
+        maxRequestSize=1024*1024*50)   // 50MB
+
+public class UploadServlet extends HttpServlet {
+    /**
+     * Name of the directory where uploaded files will be saved, relative to
+     * the web application directory.
+     */
+    private static final String SAVE_DIR = "uploadFiles";
+
+    /**
+     * handles file upload
+     */
+
+    protected void doPost(HttpServletRequest request,
+                           HttpServletResponse response) throws ServletException, IOException {
+
+        String appPath = request.getServletContext().getRealPath("");
+        // constructs path of the directory to save uploaded file
+        String savePath = appPath + File.separator + SAVE_DIR;
+
+        response.setContentType("text/html;charset=UTF-8");
+
+    // Create path components to save the file
+    final String path = request.getParameter("destination");
+    final Part filePart = request.getPart("file");
+    final String fileName = getFileName(filePart);
+
+    OutputStream out = null;
+    InputStream filecontent = null;
+    final PrintWriter writer = response.getWriter();
+
+    try {
+        out = new FileOutputStream(new File(savePath + File.separator
+                + fileName));
+        filecontent = filePart.getInputStream();
+
+        int read = 0;
+        final byte[] bytes = new byte[1024];
+
+        while ((read = filecontent.read(bytes)) != -1) {
+            out.write(bytes, 0, read);
+        }
+        writer.println("New file " + fileName + " created at " + savePath);
+
+        //call IBM service
+
+        String ibmService = "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify"
+                          + "?api_key=f18a0a0b0c92f0ba4d92aeef7c0b4bb1bbdce959"
+                          + "&url=https://drive.google.com/open?id=0B7MtYWjTFT4RS0s4RnM1NGtCbDA"
+                          + "&version=2015-12-02";
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(ibmService);
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+
+        try {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            //EntityUtils.consume(entity1);
+            String result = EntityUtils.toString(entity1);
+            writer.print(result);
+        } finally {
+            response1.close();
+        }
+
+
+
+    } catch (FileNotFoundException fne) {
+        writer.println("You either did not specify a file to upload or are "
+                + "trying to upload a file to a protected or nonexistent "
+                + "location.");
+        writer.println("<br/> ERROR: " + fne.getMessage());
+
+
+    } finally {
+        if (out != null) {
+            out.close();
+        }
+        if (filecontent != null) {
+            filecontent.close();
+        }
+        if (writer != null) {
+            writer.close();
+        }
+    }
+}
+
+    private String getFileName(final Part part) {
+        final String partHeader = part.getHeader("content-disposition");
+
+        for (String content : part.getHeader("content-disposition").split(";")) {
+            if (content.trim().startsWith("filename")) {
+                return content.substring(
+                        content.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
+        return null;
+    }
+
+
+
+    protected void doPost1(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+        // gets absolute path of the web application
+        String appPath = request.getServletContext().getRealPath("");
+        // constructs path of the directory to save uploaded file
+        String savePath = appPath + File.separator + SAVE_DIR;
+
+        // creates the save directory if it does not exists
+        File fileSaveDir = new File(savePath);
+        if (!fileSaveDir.exists()) {
+            fileSaveDir.mkdir();
+        }
+
+        for (Part part : request.getParts()) {
+            String fileName = extractFileName(part);
+            // refines the fileName in case it is an absolute path
+            fileName = new File(fileName).getName();
+            part.write(savePath + File.separator + fileName);
+        }
+        request.setAttribute("message", "Upload has been done successfully!");
+        getServletContext().getRequestDispatcher("/message.jsp").forward(
+                request, response);
+    }
+    /**
+     * Extracts file name from HTTP header content-disposition
+     */
+    private String extractFileName(Part part) {
+        String contentDisp = part.getHeader("content-disposition");
+        String[] items = contentDisp.split(";");
+        for (String s : items) {
+            if (s.trim().startsWith("filename")) {
+                return s.substring(s.indexOf("=") + 2, s.length()-1);
+            }
+        }
+        return "";
+    }
+}
